b519575495f5601f2db65a6d6d7c50570a7959ca
Mateusz Szczesniak <mszczesniak@live.com>
2021-08-26 18:54:33 +0200

    Rozwiązanie zadania 1 - Gra w zgadywanie liczb

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+computer_choice = random.randint(1,100)
+user_guess = 0
+while True:
+    try:
+        user_guess = int(input("Guess the number: "))
+        if user_guess < computer_choice:
+            print("Too small!")
+        elif user_guess > computer_choice:
+            print("Too big!")
+        elif user_guess == computer_choice:
+            print("You win!")
+            break
+    except ValueError:
+        print("It's not a number!")
